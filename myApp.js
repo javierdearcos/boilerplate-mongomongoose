db7c6648e60ba0f3382e15d5127bcfe09fc8cd42
Javier de Arcos <javier.dearcos@liferay.com>
2021-12-19 22:06:08 +0100

    Implement the queryChain method

--- myApp.js
+++ myApp.js
@@ -75,7 +75,11 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+    .sort('name')
+    .limit(2)
+    .select('-age')
+    .exec(done);
 };
 
 /** **Well Done !!**
